04d0c33f050f605361bb429dd0604458ffc76604
Liliana Beatriz Escobar <113596545+Liliana993@users.noreply.github.com>
2025-08-31 10:24:34 -0300

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import * as serviceAccount from '../key.json';
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as any),
-  databaseURL: "https://apx-base-168e8-default-rtdb.firebaseio.com"
+  databaseURL: "{YOUR-DATABASE-URL}"
 });
 
 
